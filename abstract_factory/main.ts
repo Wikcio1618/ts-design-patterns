7e20073e29049608ae99299f5738bcdea509cb84
Wikcio1618 <ciolekwiktor@gmail.com>
2025-04-11 10:41:47 +0200

    abstract factory

--- abstract_factory/main.ts
+++ abstract_factory/main.ts
@@ -1,24 +1,36 @@
-interface Widget {
-    render(): void
+abstract class Button { }
+
+abstract class Chart { }
+
+class DarkButton extends Button { }
+
+class LightButton extends Button { }
+
+class DarkChart extends Chart { }
+
+class LightChart extends Chart { }
+
+interface ThemeFactory {
+    createButton(): Button,
+    createChart(): Chart
 }
 
-class ButtonWidget implements Widget {
-    render(): void { }
+class LightFactory implements ThemeFactory {
+    createButton(): Button { return new LightButton() }
+    createChart(): Chart { return new LightChart() }
 }
 
-
-class ChartWidget implements Widget {
-    render(): void { }
+class DarkFactory implements ThemeFactory {
+    createButton(): Button { return new DarkButton() }
+    createChart(): Chart { return new DarkChart() }
 }
 
-abstract class WidgetFactory {
-    abstract createWidget(): Widget
+class Client {
+    constructor(private factory: ThemeFactory) { }
+
+    renderWidgets(): void {
+        this.factory.createButton()
+        this.factory.createChart()
+    }
 }
 
-class ButtonFactory extends WidgetFactory {
-    createWidget(): Widget { return new ButtonWidget() }
-}
-
-class ChartFactory extends WidgetFactory {
-    createWidget(): Widget { return new ButtonWidget() }
-}
